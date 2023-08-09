153d3f800078e2933ca944a67d7426c511e87931
imvee <vind.veeru@gmail.com>
2023-08-09 21:35:39 +0530

    Added helper functions

---
+++ helper.py
@@ -0,0 +1,23 @@
+import regex
+
+def date_time(s):
+    pattern = '^([0-9]+)(\/)([0-9]+)(\/)([0-9]+), ([0-9]+):([0-9]+)[ ]?(AM|PM|am|pm)? -'
+    result = regex.match(pattern, s)
+    return bool(result)
+
+def find_author(s):
+    s = s.split(":")
+    return len(s) == 2
+
+def getDatapoint(line):
+    splitline = line.split(' - ')
+    dateTime = splitline[0]
+    date, time = dateTime.split(", ")
+    message = " ".join(splitline[1:])
+    if find_author(message):
+        splitmessage = message.split(": ")
+        author = splitmessage[0]
+        message = " ".join(splitmessage[1:])
+    else:
+        author= None
+    return date, time, author, message
