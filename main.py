b96ada0f540c3f86684809da0f8a211faf6eadd9
imvee <vind.veeru@gmail.com>
2023-08-09 21:46:24 +0530

    Streamlit Init

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="WAppalyze",
+    page_icon="💬",
+    initial_sidebar_state="expanded",
+)
+
+#hide streamlit default
+hide_st_style ='''
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+'''
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.title("💬WAppalyze")
